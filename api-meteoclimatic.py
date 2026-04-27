65fa995cc9925be6495710b789510a164a055822
jantoni <ea4dqx@gmail.com>
2026-04-27 16:59:28 +0200

    Update api-meteoclimatic.py

--- api-meteoclimatic.py
+++ api-meteoclimatic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Creado por José A. García-Tenorio (jantoni) en Abril de 2026
+# Creado por José A. García Tenorio (jantoni) en Abril de 2026
 
 # Este Script envía los datos de tu estación a Meteoclimatic utilizando la versión 3 de su API.
 # Para ello necesitas definir 2 variables
