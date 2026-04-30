adb594e312c35d52d898ee1ff7925a48427adb4a
jantoni <ea4dqx@gmail.com>
2026-04-30 09:49:56 +0200

    Update api-meteoclimatic.ps1

--- api-meteoclimatic.ps1
+++ api-meteoclimatic.ps1
@@ -1,13 +1,25 @@
 # Creado a partir del script original de jantoni (Abril 2026)
 
+# Debes asegurarte que Powershell está habilitado para ejecutar scripts.
+# Ejecuta este comando dentro de Powershell
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+
+# Ejecuta el comando manualmente para asegurarte que funciona. Sitúate en el directorio donde se encuentra el script y ejecuta:
+# .\api-meteoclimatic.ps1
+# Si todo funciona correctamente, verás un mensaje con el estado 200  @{status=queued for update}
+
 # Para activar el comando cada 5 minutos, puedes utilizar el siguiente comando dentro de una ventana Powershell
 # schtasks /create /sc minute /mo 5 /tn "Meteoclimatic" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\ruta\api-meteoclimatic.ps1"
+
+# Configuración
 # Debes modificar la ruta donde se encuentra el Script.
 # Tan solo tienes que definir las siguientes variables:
 # RETRASO es el número de segundos que quieres que el script se pare antes de enviar datos a Meteoclimatic. Ajústalo en función
 # del tiempo que tarde tu software en generar ficheros para evitar enviar datos atrasados. 50 es una buena cifra para empezar
 # API_KEY define el ID API (o API Key) del usuario, no de la estación. Lo puedes encontrar en la sección "Perfil" de Meteoclimatic una vez identificado
 # DATAFILE define el fichero donde este script debe encontrar los datos a enviar a Meteoclimatic. Debes indicar la ruta y el nombre del fichero
+
+
 
 # VARIABLES A DEFINIR
 $RETRASO = 50
